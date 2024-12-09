90f9281e9983e2b14d667277a01bdacd02d75670
Sachin <sachincm10@gmail.com>
2024-12-09 15:15:02 +0530

    ui

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
         © 2024 Action Chain. Made with{" "}
         <Favorite
           sx={{
-            fontSize: "1.10rem",
+            fontSize: "1.14rem",
             color: "#ff5a5a",
             verticalAlign: "text-bottom",
           }}
