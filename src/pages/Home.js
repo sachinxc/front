1675d12bef5b2119b00552fddf354316121da28b
sachinxc <160459008+sachinxc@users.noreply.github.com>
2024-12-10 08:44:48 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,56 +1,52 @@
 import React from "react";
-import {
-  Typography,
-  Container,
-  Button,
-  Grid,
-  Card,
-  CardContent,
-  CardMedia,
-} from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { Link, Typography, Container, Button } from "@mui/material";
 
 const Home = () => {
   return (
     <div>
       {/* Main Content */}
-      <Container sx={{ marginTop: 16 }}>
-        <Typography variant="h3" component="h1" align="center" gutterBottom>
-          Welcome to the Home Page!
+      <Container sx={{ mt: 20, mb: 6 }}>
+        <Typography
+          variant="h3"
+          component="h1"
+          align="center"
+          gutterBottom
+          sx={{ color: "#000000", fontWeight: "600" }}
+        >
+          Welcome to the Action Chain!
         </Typography>
-        <Typography variant="subtitle1" align="center" gutterBottom>
-          Explore our features and learn more about what we offer.
+        <Typography
+          align="center"
+          gutterBottom
+          sx={{
+            fontFamily: "monospace",
+            fontSize: "2.5rem",
+            color: "white",
+            fontWeight: "600",
+            mt: 8,
+          }}
+        >
+          Recognize, Reward and Empower using Blockchain & AI
         </Typography>
-
-        {/* Grid of Cards */}
-        <Grid container spacing={4} sx={{ marginTop: 4 }}>
-          {Array.from({ length: 3 }).map((_, index) => (
-            <Grid item xs={12} sm={6} md={4} key={index}>
-              <Card>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={`https://source.unsplash.com/random/300x200?sig=${index}`}
-                  alt="Random"
-                />
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    Feature {index + 1}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Discover this amazing feature and see how it can benefit
-                    you.
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
 
         {/* Call to Action */}
         <Container sx={{ textAlign: "center", marginTop: 6 }}>
-          <Button variant="contained" color="secondary" size="large">
-            Get Started Now
-          </Button>
+          <Link to="/signup" component={RouterLink}>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              sx={{
+                color: "white",
+                backgroundColor: "black",
+                fontWeight: "bold",
+                fontSize: "1.4rem",
+              }}
+            >
+              Join Now!
+            </Button>
+          </Link>
         </Container>
       </Container>
     </div>
